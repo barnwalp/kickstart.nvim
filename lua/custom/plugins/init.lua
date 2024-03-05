b0e69a2b445823b08deb4050389f224534661d9c
Pankaj Barnwal <suno.pankaj@gmail.com>
2024-03-05 06:36:13 +0530

    add nvim-tree plugin and change option settings
    add option setting and nvim-tree plugin
    
    added custome plugin in the init.lua file

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,50 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+vim.keymap.set('i', 'jj', '<Esc>')
+vim.keymap.set('n', '<leader>w', ':write<CR>')
+vim.keymap.set('n', '<leader>q', ':quit<CR>')
+vim.opt.relativenumber = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+-- NeoTree Toggle config
+vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>')
+
+return {
+  {
+    'nvim-tree/nvim-tree.lua',
+    version = '*',
+    -- lazy = false,
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require('nvim-tree').setup {
+        sort_by = 'case_sensitive',
+        view = {
+          width = 23,
+        },
+        renderer = {
+          group_empty = true,
+          highlight_git = true,
+          icons = {
+            show = {
+              git = true,
+            },
+            glyphs = {
+              folder = {
+                arrow_closed = '➤',
+                -- arrow_open = '˅',
+              },
+            },
+          },
+        },
+        filters = {
+          dotfiles = true,
+        },
+      }
+    end,
+  },
+}
