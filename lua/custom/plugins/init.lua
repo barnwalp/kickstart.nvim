b43cde97eca0bcc2ecf122ecf6a984ea8185e08b
Pankaj Barnwal <suno.pankaj@gmail.com>
2025-05-22 21:56:59 +0530

    add molten plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -28,6 +28,15 @@ vim.keymap.set('n', '<leader>dq', vim.diagnostic.setloclist, { desc = 'Open diag
 vim.g.user_emmet_mode = 'a'
 
 return {
+  {
+    'benlubas/molten-nvim',
+    version = '^1.0.0', -- use version <2.0.0 to avoid breaking changes
+    build = ':UpdateRemotePlugins',
+    init = function()
+      -- this is an example, not a default. Please see the readme for more configuration options
+      vim.g.molten_output_win_max_height = 12
+    end,
+  },
   { 'edluffy/hologram.nvim' },
   {
     'dccsillag/magma-nvim',
