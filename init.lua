0269660767aa5b7f930436954d46f87925be2473
Pankaj Barnwal <suno.pankaj@gmail.com>
2025-05-25 08:53:01 +0530

    add pyright python path

--- init.lua
+++ init.lua
@@ -721,6 +721,13 @@ require('lazy').setup({
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
+            if server_name == 'pyright' then
+              server.settings = {
+                python = {
+                  pythonPath = '/Users/pankaj/anaconda3/bin/python',
+                },
+              }
+            end
             -- This handles overriding only values explicitly passed
             -- by the server configuration above. Useful when disabling
             -- certain features of an LSP (for example, turning off formatting for ts_ls)
